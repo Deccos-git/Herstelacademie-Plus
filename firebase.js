3e82deac16f8b5d998425ca9b05b1953af7fa689
Gijs Van Beusekom <gijs@MacBook-Air-van-Gijs.local>
2021-12-20 17:40:48 +0100

    landing

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdRNbWVIJHyL0AJZViKSEHtApLgT2-AqM",
+  authDomain: "herstelacademie-plus.firebaseapp.com",
+  projectId: "herstelacademie-plus",
+  storageBucket: "herstelacademie-plus.appspot.com",
+  messagingSenderId: "586569165045",
+  appId: "1:586569165045:web:f9a15725a5b63c75107dbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
